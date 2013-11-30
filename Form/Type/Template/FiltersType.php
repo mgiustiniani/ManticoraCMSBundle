20dc55ec1d90cb61292ce79c561455460a754976
Fabio Dellutri <fabio.dellutri@mitecube.com>
2013-11-30 16:10:09 +0100

    Services rationalization

--- Form/Type/Template/FiltersType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Manticora\CMSBundle\Form\Type\Template;
-
-use Admingenerated\ManticoraCMSBundle\Form\BaseTemplateType\FiltersType as BaseFiltersType;
-
-class FiltersType extends BaseFiltersType
-{
-}
